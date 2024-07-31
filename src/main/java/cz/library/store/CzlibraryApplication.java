869353ebe569fc0a637a55604a46ec28eda4d19e
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-07-30 19:42:13 -0600

    Commit inicial

---
+++ src/main/java/cz/library/store/CzlibraryApplication.java
@@ -0,0 +1,13 @@
+package cz.library.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/library/store/CzlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.library.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
