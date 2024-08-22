614c08ce90d3564603ecb4490dc455e390296c33
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-08-22 17:21:45 -0600

    refactor(user): rename and refactor UserFormatValidator
    The UserFormatValidator class is now named UserFormat.
    
    The UserFormat now extends the generic responsibility chain class Validator.
    
    Fix space issues in the UserFormat class.

--- src/main/java/cz/library/store/user/application/validation/UserFormat.java
+++ src/main/java/cz/library/store/user/application/validation/UserFormat.java
@@ -1,17 +1,17 @@
-package cz.library.store.user.application.validator.unique;
+package cz.library.store.user.application.validation;
 
-import cz.library.store.user.application.validator.UserValidator;
 import cz.library.store.user.domain.User;
+import cz.library.store.validation.domain.Validator;
 
-public class UserFormatValidator extends UserValidator{
+public class UserFormat extends Validator<User>{
 
-	@Override
-	public String validate(User user) {
+  @Override
+  public String validate(User user) {
     if (user.getEmail() != null && !user.emailIsValid()) {
-    	return "Invalid field email. The email format does not comply with RFC 2822.";
+      return "Invalid field email. The email format does not comply with RFC 2822.";
     }
     if (user.getPassword() != null && !user.passwordIsValid()) {
-    	return "Invalid field password. The password must have at least 8 characters, "
+      return "Invalid field password. The password must have at least 8 characters, "
           + "1 digit, one uppercase letter, one lowercase letter and one special character.";
     }
     if (user.getName() != null && !user.nameIsValid()) {
@@ -19,14 +19,14 @@ public class UserFormatValidator extends UserValidator{
           + "and optionally one second name.";
     }
     if (user.getLastname() != null && !user.lastnameIsValid()) {
-    	return "Invalid field lastname. The lastname should contain only "
+      return "Invalid field lastname. The lastname should contain only "
           +"letters and optionally one second lastname.";
     }
     if (user.getPhoneNumber() != null && !user.phoneNumberIsValid()) {
-    	return "Invalid field phoneNumber. The phone number can have minimum 7 digits, "
+      return "Invalid field phoneNumber. The phone number can have minimum 7 digits, "
           + "maximum 15 digits and it must have start with \"+\".";
     }
     return validateNext(user);
-	}
-  
+  }
+
 }
---
+++ src/test/java/cz/library/store/user/application/validation/UserFormatTest.java
@@ -0,0 +1,102 @@
+package cz.library.store.user.application.validation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cz.library.store.user.domain.User;
+
+public class UserFormatTest {
+
+  private UserFormat validator;
+  private User user;
+
+  @BeforeEach
+  void setUp() {
+    validator = new UserFormat();
+    user = new User();
+  }
+
+  @Test
+  void givenValidUser_whenValidate_thenReturnNull() {
+    // given
+    user.setEmail("test@example.com");
+    user.setPassword("ValidPassword1!");
+    user.setName("Jhon");
+    user.setLastname("Doe");
+    user.setPhoneNumber("+1234567890");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertNull(result);
+  }
+
+  @Test
+  void givenInvalidEmail_whenValidate_thenReturnErrorMessage() {
+    // given
+    user.setEmail("test");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertEquals("Invalid field email. The email format does not comply with RFC 2822.", result);
+  }
+
+  @Test
+  void givenInvalidPassword_whenValidate_thenReturnErrorMessage() {
+    // given
+    user.setPassword("password");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertEquals("Invalid field password. The password must have at least 8 characters, "
+        + "1 digit, one uppercase letter, one lowercase letter and one special character.", result);
+  }
+
+  @Test
+  void givenInvalidName_whenValidate_thenReturnErrorMessage() {
+    // given
+    user.setName("John Doe1");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertEquals("Invalid field name. The name should contain only letters "
+        + "and optionally one second name.", result);
+  }
+
+  @Test
+  void givenInvalidLastname_whenValidate_thenReturnErrorMessage() {
+    // given
+    user.setLastname("Doe John1");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertEquals("Invalid field lastname. The lastname should contain only "
+        +"letters and optionally one second lastname.", result);
+  }
+
+  @Test
+  void givenInvalidPhoneNumber_whenValidate_thenReturnErrorMessage() {
+    // given
+    user.setPhoneNumber("12345678901");
+
+    // when
+    String result = validator.validate(user);
+
+    // then
+    assertEquals("Invalid field phoneNumber. The phone number can have minimum 7 digits, "
+        + "maximum 15 digits and it must have start with \"+\".", result);
+  }
+
+}
