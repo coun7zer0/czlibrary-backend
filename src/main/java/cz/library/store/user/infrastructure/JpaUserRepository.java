6904fc190a3a1676da7b24939e1a02e2402553e6
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-08-23 16:02:46 -0600

    feat(user): add JpaUserRepository
    The JpaUserRepository is the repository for UserDataMapper.

---
+++ src/main/java/cz/library/store/user/infrastructure/JpaUserRepository.java
@@ -0,0 +1,8 @@
+package cz.library.store.user.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaUserRepository extends JpaRepository<UserDataMapper, Long> {
+}
