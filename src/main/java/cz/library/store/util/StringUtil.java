e76b3f965525321e78c11f3274e0ced786178db0
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-08-23 20:02:29 -0600

    style: fix space issues

--- src/main/java/cz/library/store/util/StringUtil.java
+++ src/main/java/cz/library/store/util/StringUtil.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public final class StringUtil {
-  
+
   public static String camelCaseToWords(String str) {
     return str
         .replaceAll("([a-z])([A-Z])", "$1 $2")
@@ -17,7 +17,7 @@ public final class StringUtil {
     if(words.size() == 1) {
       return words.get(0);
     }
-    
+
     return words.subList(0, words.size() - 1).stream()
         .collect(Collectors.joining(", "))
         .toString() + " or " + words.get(words.size() - 1);
