3489b7186b0063e3d5e8942df877117e17803bd2
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-08-26 19:03:52 -0600

    refactor(user): simplify UserCreateValidation
    Refactor `UserCreateValidation` to use `UserValidatorBuilder` for
    constructing the validation chain. This change reduces boilerplate
    code and improves readability.

--- src/main/java/cz/library/store/user/application/usecase/create/UserCreateValidation.java
+++ src/main/java/cz/library/store/user/application/usecase/create/UserCreateValidation.java
@@ -1,52 +1,32 @@
 package cz.library.store.user.application.usecase.create;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import cz.library.store.user.application.UserDataSourceGateway;
-import cz.library.store.user.application.validation.UserFormat;
+import cz.library.store.user.application.validation.UserValidatorBuilder;
 import cz.library.store.user.domain.User;
-import cz.library.store.validation.application.FieldIsUnique;
-import cz.library.store.validation.application.FieldNotNull;
-import cz.library.store.validation.application.FieldNotNullOr;
-import cz.library.store.validation.domain.Validator;
 
 public class UserCreateValidation {
 
-  private final UserDataSourceGateway userDataSource;
+  private final UserCreateDataSourceGateway userDataSource;
 
-  public UserCreateValidation(UserDataSourceGateway userDataSource) {
+  public UserCreateValidation(UserCreateDataSourceGateway userDataSource) {
     this.userDataSource = userDataSource;
   }
 
   public String validate(User user) {
-    List<Validator<User>> validations = new ArrayList<>();
-
-    validations.add(
-        new FieldNotNull<>(
-            "password", User::getPassword));
-
-    validations.add(
-        new FieldNotNullOr<>(new HashMap<>(Map.of(
+    return new UserValidatorBuilder()
+        .fieldNotNull(
+            "password", User::getPassword)
+        .fieldNotNullOr(Map.of(
             "email", User::getEmail,
-            "phoneNumber", User::getPhoneNumber))));
-
-    validations.add(
-        new UserFormat());
-
-    validations.add(
-        new FieldIsUnique<>(
-            "email", User::getEmail, userDataSource::existsByEmail));
-
-    validations.add(
-        new FieldIsUnique<>(
-            "phoneNumber", User::getPhoneNumber, userDataSource::existsByPhoneNumber));
-
-    Validator<User> validatorChain = Validator.link(validations);
-
-    return validatorChain.validate(user);
+            "phoneNumber", User::getPhoneNumber))
+        .userFormat()
+        .fieldIsUnique(
+            "email", User::getEmail, userDataSource::existsByEmail)
+        .fieldIsUnique(
+            "phoneNumber", User::getPhoneNumber, userDataSource::existsByPhoneNumber)
+        .build()
+        .validate(user);
   }
 
 }
