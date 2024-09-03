6a062daa3bce86f8c408f1cc4244873e44e3d06a
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-09-03 14:28:08 -0600

    feat(application): customize scanning to exclude certain packages
    Added a custom filter in `CzlibraryApplication` to exclude packages
    containing 'dto', 'validation', 'infrastructure', and 'domain' during
    component scanning.
    
    Ensured that only relevant beans are included in the Spring context by
    implementing `RemoveModelAndEntitiesFilter`.

--- src/main/java/cz/library/store/CzlibraryApplication.java
+++ src/main/java/cz/library/store/CzlibraryApplication.java
@@ -2,12 +2,33 @@ package cz.library.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
 
 @SpringBootApplication
+@ComponentScan(
+    basePackages = "cz.library.store",
+    includeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = RemoveModelAndEntitiesFilter.class)
+)
 public class CzlibraryApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CzlibraryApplication.class, args);
   }
 
+}
+
+class RemoveModelAndEntitiesFilter implements TypeFilter {
+
+  @Override
+  public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) {
+    String className = metadataReader.getClassMetadata().getClassName();
+    return !className.contains("dto")
+        && !className.contains("validation")
+        && !className.contains("infrastructure")
+        && !className.contains("domain");
+  }
 }
