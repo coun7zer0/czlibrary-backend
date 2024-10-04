fb7aa7d0cfcfc2936c7bc8ba4afee9dd4a27c7cf
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-10-04 15:50:53 -0600

    feat(security): enhance security configuration in SecurityConfig.java
    Added support for basic authentication by allowing all requests to
    /login and /api/v1/users.
    
    Introduced AuthenticationManager bean for authentication management.
    
    Added PasswordEncoder bean using BCryptPasswordEncoder for password
    encoding.

--- src/main/java/cz/library/store/security/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/cz/library/store/security/infrastructure/configuration/SecurityConfig.java
@@ -3,9 +3,14 @@ package cz.library.store.security.infrastructure.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -24,8 +29,20 @@ public class SecurityConfig {
             authorize -> authorize
                 .requestMatchers("/login").permitAll()
                 .requestMatchers("/api/v1/users").permitAll()
-                .anyRequest().denyAll());
+                .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults()); // this is for activate basic auth only
 
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
