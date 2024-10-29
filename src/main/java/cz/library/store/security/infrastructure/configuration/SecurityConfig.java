728b91478348b2970a3eb8e609f1bc29773102e5
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-10-29 16:37:32 -0600

    feat(security): enhance security configuration with JWT and custom password encoder
    Added dependency injection for `JwtService` and `BCryptEncoder` to
    improve token and password handling.
    
    Integrated `JwtAuthenticationFilter` in `SecurityConfig` to manage
    JWT-based authentication.
    
    Updated `SecurityConfig` and `TestSecurityConfig` to use
    `TokenProvider` and `PasswordEncoder` beans.
    
    Enabled JWT authorization for `/login` and secured remaining endpoints
    by default.

--- src/main/java/cz/library/store/security/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/cz/library/store/security/infrastructure/configuration/SecurityConfig.java
@@ -1,22 +1,38 @@
 package cz.library.store.security.infrastructure.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import cz.library.store.security.application.service.PasswordEncoder;
+import cz.library.store.security.application.service.TokenProvider;
+import cz.library.store.security.infrastructure.filters.jwt.JpaJwtFilter;
+import cz.library.store.security.infrastructure.filters.jwt.JwtAuthenticationFilter;
+import cz.library.store.security.infrastructure.service.BCryptEncoder;
+import cz.library.store.security.infrastructure.service.JwtService;
 
 @EnableWebSecurity
 @Configuration
 @Profile("!test")
 public class SecurityConfig {
+
+  @Value("${api.security.secret}")
+  private String apiSecret;
+
+  private final JpaJwtFilter jwtDataSource;
+
+  public SecurityConfig(JpaJwtFilter jwtDataSource) {
+    this.jwtDataSource = jwtDataSource;
+  }
 
   @Bean
   public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
@@ -27,10 +43,12 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             authorize -> authorize
-                .requestMatchers("/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .requestMatchers("/api/v1/users").permitAll()
                 .anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults()); // this is for activate basic auth only
+        .addFilterBefore(
+            new JwtAuthenticationFilter(TokenProvider(), jwtDataSource),
+            UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
@@ -43,6 +61,11 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+    return new BCryptEncoder();
+  }
+
+  @Bean
+  public TokenProvider TokenProvider() {
+    return new JwtService(apiSecret);
   }
 }
--- src/main/java/cz/library/store/security/infrastructure/configuration/TestSecurityConfig.java
+++ src/main/java/cz/library/store/security/infrastructure/configuration/TestSecurityConfig.java
@@ -1,5 +1,6 @@
 package cz.library.store.security.infrastructure.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,10 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import cz.library.store.security.application.service.PasswordEncoder;
+import cz.library.store.security.application.service.TokenProvider;
+import cz.library.store.security.infrastructure.service.BCryptEncoder;
+import cz.library.store.security.infrastructure.service.JwtService;
+
 @EnableWebSecurity
 @Configuration
 @Profile("test")
 public class TestSecurityConfig {
+
+  @Value("${api.security.secret}")
+  private String apiSecret;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,4 +32,15 @@ public class TestSecurityConfig {
 
     return http.build();
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptEncoder();
+  }
+
+  @Bean
+  public TokenProvider tokenProvider() {
+    return new JwtService(apiSecret);
+  }
+
 }
