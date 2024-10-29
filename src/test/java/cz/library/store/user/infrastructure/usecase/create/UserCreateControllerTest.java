b42367d001a3dac5c7e71ea3aafc5996dcebd42d
William Christopher Rendon Castruita <coun7zer0@proton.me>
2024-10-29 17:35:31 -0600

    test(user): enhance UserCreateControllerTest with token validation
    Added token validation in user creation tests.
    
    Modified existing test to use a valid token, ensuring authorization
    for user creation.
    
    Introduced a new test case for handling invalid tokens, verifying that
    unauthorized requests are correctly rejected.
    
    Refactored test setup to initialize mock user data and clear the user
    repository before each test.

--- src/test/java/cz/library/store/user/infrastructure/usecase/create/UserCreateControllerTest.java
+++ src/test/java/cz/library/store/user/infrastructure/usecase/create/UserCreateControllerTest.java
@@ -11,11 +11,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.net.URI;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,10 +26,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import cz.library.store.security.application.service.TokenProvider;
 import cz.library.store.user.application.dto.UserRequestData;
 import cz.library.store.user.application.dto.UserResponseData;
 import cz.library.store.user.domain.Gender;
 import cz.library.store.user.domain.User;
+import cz.library.store.user.infrastructure.persistence.JpaUserRepository;
+import cz.library.store.user.infrastructure.persistence.UserDataMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -40,47 +45,93 @@ public class UserCreateControllerTest {
   @MockBean
   private UserCreateService userService;
 
+  private User mockUser;
+
   @Autowired
   private ObjectMapper objectMapper;
 
+  @Autowired
+  private TokenProvider tokenProvider;
+
+  @Autowired
+  private JpaUserRepository userRepository;
+
+  @BeforeEach
+  void setUp() {
+    userRepository.deleteAll();
+    mockUser = new User(
+        1L,
+        "jhon.doe@example.com",
+        "ValidPassword123!",
+        "Jhon",
+        "Doe",
+        "+123456789",
+        Gender.MALE);
+  }
+
   @Test
-  void givenValidUserData_whenCreate_thenReturnCreatedResponseWithLocation()
+  void givenValidUserDataAndValidToken_whenCreate_thenReturnCreatedResponseWithLocation()
       throws Exception {
     // given
-    User user = new User(
-        1L, "jhon.doe@example.com", "ValidPassword123!", "Jhon", "Doe", "+123456789", Gender.MALE);
-
+    userRepository.save(new UserDataMapper(this.mockUser));
     UserRequestData request = new UserRequestData(
-        user.getEmail(),
-        user.getPassword(),
-        user.getName(),
-        user.getLastname(),
-        user.getPhoneNumber(),
-        user.getGender());
+        mockUser.getEmail(),
+        mockUser.getPassword(),
+        mockUser.getName(),
+        mockUser.getLastname(),
+        mockUser.getPhoneNumber(),
+        mockUser.getGender());
 
-    UserResponseData response = new UserResponseData(user);
+    UserResponseData response = new UserResponseData(mockUser);
 
     String expectedLocation = "/api/v1/users/1";
 
     when(userService.create(any(UserRequestData.class), any(UriComponentsBuilder.class)))
         .thenReturn(ResponseEntity.created(new URI(expectedLocation)).body(response));
 
+    String token = tokenProvider.generateToken(mockUser, mockUser.getEmail());
+
     // when
     mockMvc.perform(post("/api/v1/users")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(request)))
+        .content(objectMapper.writeValueAsString(request))
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
 
     // then
         .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").value(1L))
-        .andExpect(jsonPath("$.email").value("jhon.doe@example.com"))
-        .andExpect(jsonPath("$.name").value("Jhon"))
-        .andExpect(jsonPath("$.lastname").value("Doe"))
-        .andExpect(jsonPath("$.phoneNumber").value("+123456789"))
-        .andExpect(jsonPath("$.gender").value("MALE"))
+        .andExpect(jsonPath("$.id").value(mockUser.getId()))
+        .andExpect(jsonPath("$.email").value(mockUser.getEmail()))
+        .andExpect(jsonPath("$.name").value(mockUser.getName()))
+        .andExpect(jsonPath("$.lastname").value(mockUser.getLastname()))
+        .andExpect(jsonPath("$.phoneNumber").value(mockUser.getPhoneNumber()))
+        .andExpect(jsonPath("$.gender").value(mockUser.getGender().toString()))
         .andExpect(header().string("Location", expectedLocation));
 
     verify(userService, times(1)).create(any(UserRequestData.class), any(UriComponentsBuilder.class));
+  }
+
+  @Test
+  void givenValidUserDataAndInvalidToken_whenCreate_thenReturnUnauthorizedResponse() throws Exception {
+    // given
+    UserRequestData request = new UserRequestData(
+        mockUser.getEmail(),
+        mockUser.getPassword(),
+        mockUser.getName(),
+        mockUser.getLastname(),
+        mockUser.getPhoneNumber(),
+        mockUser.getGender());
+
+    // Generar un token inválido
+    String invalidToken = "invalid.token.here";
+
+    // when
+    mockMvc.perform(post("/api/v1/users")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request))
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + invalidToken))
+
+        // then
+        .andExpect(status().isForbidden());
   }
 
 }
